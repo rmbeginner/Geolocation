cf8920ae0d87f24feb2802f4bec748f3a925100d
UserIT <UserIT@ITADMIN>
2023-01-05 10:26:15 +0800

    Add project files.

---
+++ Geolocation/Geolocation/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials; 
+
+namespace Geolocation
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var location = await Xamarin.Essentials.Geolocation.GetLastKnownLocationAsync();
+                if (location == null)
+                {
+                    location = await Xamarin.Essentials.Geolocation.GetLocationAsync(new GeolocationRequest
+                    {
+                        DesiredAccuracy = GeolocationAccuracy.Medium,
+                        Timeout = TimeSpan.FromSeconds(30)
+                    });
+                }
+
+                if(location == null)
+                    LabelLocation.Text = "NO GPS";
+                else
+                    LabelLocation.Text = $"{location.Latitude}, {location.Longitude}";
+
+            } catch (Exception ex)
+            {
+                Debug.WriteLine($"Someting is wrong: {ex.Message}");  
+            }
+        }
+    }
+}
